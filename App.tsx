af6268b68b4486504bdfba784b42362c4590d0bf
Cintia <cintiaschutt@gmail.com>
2025-03-11 17:18:52 -0600

    react native course

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+/*
+  view is same as div
+  all text must be surround by a text tag
+  all styling in React Native is using flexbox, it's similar to the web, but some defaults are different
+*/
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          backgroundColor: "pink",
+          paddingHorizontal: 8,
+          paddingVertical: 16,
+        }}
+      >
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center",
   },
 });
